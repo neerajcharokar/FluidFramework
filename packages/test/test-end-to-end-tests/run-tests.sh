63ae977c0a6fa63dece10b81ae22afa0e88156c5
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-10-03 14:57:03 -0700

    Stop using /tmp

--- packages/test/test-end-to-end-tests/run-tests.sh
+++ packages/test/test-end-to-end-tests/run-tests.sh
@@ -1,21 +1,18 @@
-mkdir -p /tmp/testRuns
+mkdir -p ./testRuns
 
 STATUS=0
 COUNTER=0
 while [ $STATUS -eq 0 ]
 do
 	# Clean up log files so we don't keep increasing the used space. Ignore if it fails (if there are no files).
-	rm /tmp/testRuns/* > /dev/null
+	rm ./testRuns/* > /dev/null
 
 	COUNTER=$((COUNTER+1))
 	export TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 	OUTPUT_FILE=$TIMESTAMP.log
-	echo "Running tests ($COUNTER)...output file: /tmp/testRuns/$OUTPUT_FILE"
-	npm run test:realsvc:tinylicious > /tmp/testRuns/$OUTPUT_FILE 2>&1
+	echo "Running tests ($COUNTER)...output file: ./testRuns/$OUTPUT_FILE"
+	npm run test:realsvc:tinylicious > ./testRuns/$OUTPUT_FILE 2>&1
 	STATUS=$?
 done
-
-# Copy the files back because /tmp gets regenerated when the container starts
-cp /tmp/testRuns/${TIMESTAMP}* .
 
 echo "Tests failed with status $STATUS"
