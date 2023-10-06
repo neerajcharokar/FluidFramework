c19c6e55e3fbda30e428a5b495ad2a065fc40232
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-10-05 17:01:59 -0700

    Cleanup persistent store for t9s in between test runs

--- packages/test/test-end-to-end-tests/run-tests.sh
+++ packages/test/test-end-to-end-tests/run-tests.sh
@@ -6,6 +6,7 @@ while [ $STATUS -eq 0 ]
 do
 	# Clean up log files so we don't keep increasing the used space. Ignore if it fails (if there are no files).
 	rm ./testRuns/* > /dev/null
+	rm -rf /var/tmp/tinylicious/ > /dev/null
 
 	COUNTER=$((COUNTER+1))
 	export TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
