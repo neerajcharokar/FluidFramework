f02f2a75f9ca184374e7b222ab8fb1adb8578012
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-11-14 15:27:41 -0500

    Improve run-test.sh

--- packages/test/test-end-to-end-tests/run-tests.sh
+++ packages/test/test-end-to-end-tests/run-tests.sh
@@ -4,15 +4,18 @@ STATUS=0
 COUNTER=0
 while [ $STATUS -eq 0 ]
 do
-	# Clean up log files so we don't keep increasing the used space. Ignore if it fails (if there are no files).
+	# Clean up output files from previous runs so we keep used space under control. Ignore failures (if there are no files).
 	rm ./testRuns/* > /dev/null
+	# Clean up tinylicious data to keep used space under control. Ignore failures (if there are no files).
 	rm -rf /var/tmp/tinylicious/ > /dev/null
 
 	COUNTER=$((COUNTER+1))
-	export TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+	TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 	OUTPUT_FILE=$TIMESTAMP.log
+
 	echo "Running tests ($COUNTER)...output file: ./testRuns/$OUTPUT_FILE"
-	npm run test:realsvc:tinylicious > ./testRuns/$OUTPUT_FILE 2>&1
+	# npm run test:realsvc:tinylicious > ./testRuns/$OUTPUT_FILE 2>&1
+	npm run test:realsvc:routerlicious:report --compatVersion=0 > ./testRuns/$OUTPUT_FILE 2>&1
 	STATUS=$?
 done
 
