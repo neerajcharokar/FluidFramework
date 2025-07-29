72edebd7b11025c1d81282a0f3af7466086a2b0b
charokn <neeraj.charokar@autodesk.com>
2025-07-29 10:17:37 +0530

    added log in responseSizeMiddleware

--- server/routerlicious/packages/services-utils/src/responseSizeMiddleware.ts
+++ server/routerlicious/packages/services-utils/src/responseSizeMiddleware.ts
@@ -15,6 +15,7 @@ export class ResponseSizeMiddleware {
 			const originalSend = res.send;
 			res.send = (body) => {
 				let responseSize: number;
+				console.log("responseSizeMiddleware body", body);
 				try {
 					responseSize = Buffer.byteLength(
 						typeof body === "string" ? body : JSON.stringify(body),
