c053d06b3f13053b84a962b2823f78485f893d7b
charokn <neeraj.charokar@autodesk.com>
2025-07-31 13:01:10 +0530

    Update responseSizeMiddleware.ts

--- server/routerlicious/packages/services-utils/src/responseSizeMiddleware.ts
+++ server/routerlicious/packages/services-utils/src/responseSizeMiddleware.ts
@@ -21,7 +21,7 @@ export class ResponseSizeMiddleware {
 						typeof body === "string" ? body : JSON.stringify(body),
 					);
 				} catch (error) {
-					Lumberjack.error(`Invalid JSON string in response body: ${body} <<<<<<<<`, undefined, error);
+					Lumberjack.error(`Test JSON string in response body: ${body} <<<<<<<<`, undefined, error);
 					// In case of JSON parsing errors, we log internally and send the
 					// original response to the client to prevent breaking the client's experience.
 					return originalSend.call(res, body);
